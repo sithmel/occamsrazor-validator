27c7d471f4542ad9c4e1e8f638656deea060a078
sithmel <maurizio.lupo@gmail.com>
2019-11-26 15:04:22 +0000

    refresh dependencies

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-var match = require('occamsrazor-match');
-var and = require('occamsrazor-match/extra/and');
-var combineValidators = require('./lib/combine-validators');
-var ValidationResult = require('./lib/validation-result');
-var setName = require('occamsrazor-match/lib/setName');
+var match = require('occamsrazor-match')
+var and = require('occamsrazor-match/extra/and')
+var combineValidators = require('./lib/combine-validators')
+var ValidationResult = require('./lib/validation-result')
+var setName = require('occamsrazor-match/lib/setName')
 
+function buildValidator (baseScore, funcs) {
+  baseScore = baseScore || 0
+  funcs = funcs || []
+  var innerValidator = and(funcs)
 
-function buildValidator(baseScore, funcs) {
-  baseScore = baseScore || 0;
-  funcs = funcs || [];
-  var innerValidator = and(funcs);
-
-  function validator(obj, callback) {
+  function validator (obj, callback) {
     if (innerValidator(obj, callback)) {
-      return new ValidationResult(funcs.length + baseScore, obj);
+      return new ValidationResult(funcs.length + baseScore, obj)
     }
-    return null;
+    return null
   };
 
   validator.score = function () {
-    return funcs.length + baseScore;
-  };
+    return funcs.length + baseScore
+  }
 
   validator.match = function (func) {
-    return buildValidator(baseScore, funcs.concat(match(func)));
-  };
+    return buildValidator(baseScore, funcs.concat(match(func)))
+  }
 
   validator.important = function (bump) {
-    bump = bump || 64;
-    return buildValidator(baseScore + bump, funcs);
-  };
+    bump = bump || 64
+    return buildValidator(baseScore + bump, funcs)
+  }
 
-  return setName(validator, innerValidator.name + ' (score: ' + validator.score() + ')');
+  return setName(validator, innerValidator.name + ' (score: ' + validator.score() + ')')
 }
 
-buildValidator.combine = combineValidators;
+buildValidator.combine = combineValidators
 
-module.exports = buildValidator;
+module.exports = buildValidator
